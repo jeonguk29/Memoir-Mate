a24497a6747ed97a90668b19edb29761c7896475
jeonguk29 <jeonguk29@naver.com>
2023-12-17 17:00:49 +0900

    feat : appleLogin 구현 완료
    https://doitduri.tistory.com/66
    참고

--- Memoir-Mate/Memoir-Mate/View/Controller/Login/LoginViewController.swift
+++ Memoir-Mate/Memoir-Mate/View/Controller/Login/LoginViewController.swift
@@ -10,10 +10,12 @@ import FirebaseAuth
 import FirebaseCore
 import FirebaseDatabase
 import UIKit
-import AuthenticationServices
+import AuthenticationServices // apple login 관련 라이브러리
+import CryptoKit // 해시 값 추가
 
 @available(iOS 16.0, *)
 class LoginViewController: UIViewController {
+    
     
     
     let backgroundImage = UIImage(named: "Login3") // 'your_image_name' 대신 실제 이미지 파일 이름 입력
@@ -37,8 +39,26 @@ class LoginViewController: UIViewController {
     
     
     
+    
     //애플 로그인
     
+    private let appleButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign in with Apple", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17)
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8
+        button.clipsToBounds = true
+        button.addTarget(self, action: #selector(tapAppleLoginButton), for: .touchUpInside)
+        return button
+    }()
+    
+    var currentNonce: String? = ""
+    
+    @objc func tapAppleLoginButton(_ sender: UIButton) {
+        startSignInWithAppleFlow()
+    }
     
     
     @objc func handleGoogleLogin(){
@@ -151,6 +171,9 @@ class LoginViewController: UIViewController {
         googleButton.translatesAutoresizingMaskIntoConstraints = false
         
         
+        backgroundImageView.addSubview(appleButton) // 필요한 경우 버튼을 뷰에 추가
+        appleButton.translatesAutoresizingMaskIntoConstraints = false
+        
         //emailInfoLabel.leadingAnchor.constraint(equalTo: emailTextFieldView.leadingAnchor, constant: 8).isActive = true
              // 이런식으로 .isActive = true 끝에 다 활성화 붙일 필요 없음
         NSLayoutConstraint.activate([
@@ -172,11 +195,133 @@ class LoginViewController: UIViewController {
                 // Set leading and trailing constraints for the button with 30 points padding from screen edges.
                 googleButton.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 40),
                 googleButton.trailingAnchor.constraint(equalTo: backgroundImageView.trailingAnchor, constant: -40),
-
+                
+                appleButton.topAnchor.constraint(equalTo: googleButton.bottomAnchor, constant: 20),
+                appleButton.leadingAnchor.constraint(equalTo: backgroundImageView.leadingAnchor, constant: 40),
+                appleButton.trailingAnchor.constraint(equalTo: backgroundImageView.trailingAnchor, constant: -40),
+                
              ])
     }
     
     
     
     
+}
+
+
+
+
+
+@available(iOS 16.0, *)
+extension LoginViewController {
+    func startSignInWithAppleFlow() {
+           let nonce = randomNonceString()
+           currentNonce = nonce
+           let appleIDProvider = ASAuthorizationAppleIDProvider()
+           let request = appleIDProvider.createRequest()
+           // request 요청을 했을 때 none가 포함되어서 릴레이 공격을 방지
+           // 추후 파베에서도 무결성 확인을 할 수 있게끔 함
+           request.requestedScopes = [.fullName, .email]
+           request.nonce = sha256(nonce)
+           
+           let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+           authorizationController.delegate = self
+           authorizationController.presentationContextProvider = self
+           authorizationController.performRequests()
+       }
+       
+       private func sha256(_ input: String) -> String {
+           let inputData = Data(input.utf8)
+           let hashedData = SHA256.hash(data: inputData)
+           let hashString = hashedData.compactMap {
+               return String(format: "%02x", $0)
+           }.joined()
+           
+           return hashString
+       }
+       
+       // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+       private func randomNonceString(length: Int = 32) -> String {
+           precondition(length > 0)
+           let charset: Array<Character> =
+               Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+           var result = ""
+           var remainingLength = length
+           
+           while remainingLength > 0 {
+               let randoms: [UInt8] = (0 ..< 16).map { _ in
+                   var random: UInt8 = 0
+                   let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                   if errorCode != errSecSuccess {
+                       fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                   }
+                   return random
+               }
+               
+               randoms.forEach { random in
+                   if remainingLength == 0 {
+                       return
+                   }
+                   
+                   if random < charset.count {
+                       result.append(charset[Int(random)])
+                       remainingLength -= 1
+                   }
+               }
+           }
+           
+           return result
+       }
+}
+
+@available(iOS 16.0, *)
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    
+    // controller로 인증 정보 값을 받게 되면은, idToken 값을 받음
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            
+            // nonce : 암호화된 임의의 난수, 단 한번만 사용 가능
+            // 동일한 요청을 짧은 시간에 여러번 보내는 릴레이 공격 방지
+            // 정보 탈취 없이 안전하게 인증 정보 전달을 위한 안전장치
+            
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+                // 안전하게 인증 정보를 전달하기 위해 nonce 사용
+            
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            
+            // token들로 credential을 구성해서 auth signin 구성 (google과 동일)
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print ("Error Apple sign in: %@", error)
+                    return
+                }
+                // User is signed in to Firebase with Apple.
+                // ...
+                ///Main 화면으로 보내기
+                let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+                let mainViewController = storyboard.instantiateViewController(identifier: "MainViewController")
+                mainViewController.modalPresentationStyle = .fullScreen
+                self.navigationController?.show(mainViewController, sender: nil)
+            }
+        }
+    }
+}
+
+@available(iOS 16.0, *)
+extension LoginViewController : ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
 }
