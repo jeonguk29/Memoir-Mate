ad27b04f95c0c077e297f2a11e69e0dda4873249
jeonguk29 <jeonguk29@naver.com>
2023-12-31 20:52:42 +0900

    feat : Apple Face ID Login 동의 얻기 구현

--- Memoir-Mate/Memoir-Mate/View/Controller/Login/LoginViewController.swift
+++ Memoir-Mate/Memoir-Mate/View/Controller/Login/LoginViewController.swift
@@ -15,6 +15,7 @@ import CryptoKit // 해시 값 추가
 import SwiftUI
 import AVFoundation
 import AVKit
+import LocalAuthentication
 
 @available(iOS 16.0, *)
 class LoginViewController: UIViewController {
@@ -175,7 +176,7 @@ class LoginViewController: UIViewController {
     
     
     private func setupAutoLayout() {
-  
+        
         // 비디오 파일 경로를 가져옵니다.
         if let videoPath = Bundle.main.path(forResource: "LoginView", ofType: "mp4") {
             // AVPlayer 인스턴스를 생성합니다.
@@ -198,7 +199,7 @@ class LoginViewController: UIViewController {
                 player?.seek(to: CMTime.zero) // 비디오를 처음으로 되감습니다.
                 player?.play() // 비디오를 재생합니다.
             }
-                   
+            
             
             player.isMuted = true
             // 비디오 재생을 시작합니다.
@@ -206,24 +207,24 @@ class LoginViewController: UIViewController {
             
             // collectionView의 배경으로 비디오 뷰를 설정합니다.
             self.view.addSubview(videoView)
-                    
+            
             
             videoView.addSubview(googleButton)
             googleButton.translatesAutoresizingMaskIntoConstraints = false
-
+            
             videoView.addSubview(appleButton)
             appleButton.translatesAutoresizingMaskIntoConstraints = false
-
+            
             NSLayoutConstraint.activate([
                 // Set height of the button
                 googleButton.heightAnchor.constraint(equalToConstant: 40),
                 appleButton.heightAnchor.constraint(equalToConstant: 40),
-
+                
                 // Center the buttons in the transparentView
                 googleButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 480),
                 googleButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
                 googleButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
-         
+                
                 // Set vertical spacing between the buttons
                 appleButton.topAnchor.constraint(equalTo: googleButton.bottomAnchor, constant: 20),
                 appleButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 42),
@@ -231,9 +232,9 @@ class LoginViewController: UIViewController {
             ])
         }
         
-
+        
     }
-
+    
 }
 
 
@@ -241,64 +242,90 @@ class LoginViewController: UIViewController {
 @available(iOS 16.0, *)
 extension LoginViewController {
     func startSignInWithAppleFlow() {
-           let nonce = randomNonceString()
-           currentNonce = nonce
-           let appleIDProvider = ASAuthorizationAppleIDProvider()
-           let request = appleIDProvider.createRequest()
-           // request 요청을 했을 때 none가 포함되어서 릴레이 공격을 방지
-           // 추후 파베에서도 무결성 확인을 할 수 있게끔 함
-           request.requestedScopes = [.fullName, .email]
-           request.nonce = sha256(nonce)
-           
-           let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-           authorizationController.delegate = self
-           authorizationController.presentationContextProvider = self
-           authorizationController.performRequests()
-       }
-       
-       private func sha256(_ input: String) -> String {
-           let inputData = Data(input.utf8)
-           let hashedData = SHA256.hash(data: inputData)
-           let hashString = hashedData.compactMap {
-               return String(format: "%02x", $0)
-           }.joined()
-           
-           return hashString
-       }
-       
-       // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
-       private func randomNonceString(length: Int = 32) -> String {
-           precondition(length > 0)
-           let charset: Array<Character> =
-               Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-           var result = ""
-           var remainingLength = length
-           
-           while remainingLength > 0 {
-               let randoms: [UInt8] = (0 ..< 16).map { _ in
-                   var random: UInt8 = 0
-                   let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-                   if errorCode != errSecSuccess {
-                       fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
-                   }
-                   return random
-               }
-               
-               randoms.forEach { random in
-                   if remainingLength == 0 {
-                       return
-                   }
-                   
-                   if random < charset.count {
-                       result.append(charset[Int(random)])
-                       remainingLength -= 1
-                   }
-               }
-           }
-           
-           return result
-       }
+        let context = LAContext()
+        var error: NSError?
+        
+        // Face ID가 사용 가능하고 권한이 부여된 경우 확인
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Face ID로 원활한 로그인을 위해 인증하세요."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self.performAppleSignIn()
+                    } else {
+                        // 인증 실패 또는 사용자 취소 처리
+                        print("Face ID 인증 실패 또는 취소됨.")
+                    }
+                }
+            }
+        } else {
+            // Face ID를 사용할 수 없으면 직접 Apple Sign-In 수행
+            performAppleSignIn()
+        }
+    }
+    
+    private func performAppleSignIn() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    // ... (나머지 코드는 변경되지 않음)
 }
+
+
+private func sha256(_ input: String) -> String {
+    let inputData = Data(input.utf8)
+    let hashedData = SHA256.hash(data: inputData)
+    let hashString = hashedData.compactMap {
+        return String(format: "%02x", $0)
+    }.joined()
+    
+    return hashString
+}
+
+// Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+private func randomNonceString(length: Int = 32) -> String {
+    precondition(length > 0)
+    let charset: Array<Character> =
+    Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+    var result = ""
+    var remainingLength = length
+    
+    while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+            var random: UInt8 = 0
+            let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+            if errorCode != errSecSuccess {
+                fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+            }
+            return random
+        }
+        
+        randoms.forEach { random in
+            if remainingLength == 0 {
+                return
+            }
+            
+            if random < charset.count {
+                result.append(charset[Int(random)])
+                remainingLength -= 1
+            }
+        }
+    }
+    
+    return result
+}
+
 
 @available(iOS 16.0, *)
 extension LoginViewController: ASAuthorizationControllerDelegate {
@@ -314,7 +341,7 @@ extension LoginViewController: ASAuthorizationControllerDelegate {
             guard let nonce = currentNonce else {
                 fatalError("Invalid state: A login callback was received, but no login request was sent.")
                 // 안전하게 인증 정보를 전달하기 위해 nonce 사용
-            
+                
             }
             guard let appleIDToken = appleIDCredential.identityToken else {
                 print("Unable to fetch identity token")
@@ -336,63 +363,63 @@ extension LoginViewController: ASAuthorizationControllerDelegate {
                 // User is signed in to Firebase with Apple.
                 print("apple Login Test \(authResult?.user.uid)")
                 UserService.shared.LoginfetchUser(uid: authResult?.user.uid ?? "deleteUser") { fetchedUser in
-                              let uid = authResult?.user.uid
-                              let email: String
-                              let userName: String
-                              let userNickName: String
-                              let photoURLString: String?
-                              let userSetting: Bool
-                              let bio: String
-                              let backgroundCustomImage1: String
-                              let backgroundCustomImage2: String
-
-                              if let user = fetchedUser {
-                                  // 사용자 정보가 이미 있는 경우
-                                  email = user.email
-                                  userName = user.username
-                                  userNickName = user.userNickName
-                                  photoURLString = user.photoURLString?.absoluteString
-                                  userSetting = user.userSetting
-                                  bio = user.bio ?? "Memoir Mate User"
-                                  backgroundCustomImage1 = user.backgroundCustomImage1?.absoluteString ?? ""
-                                  backgroundCustomImage2 = user.backgroundCustomImage2?.absoluteString ?? ""
-                              } else {
-                                  // 처음 로그인하는 사용자의 경우
-                                  email = authResult?.user.email ?? ""
-                                  userName = authResult?.user.displayName ?? ""
-                                  userNickName = ""
-                                  photoURLString = authResult?.user.photoURL?.absoluteString
-                                  userSetting = false
-                                  bio = "Memoir Mate User"
-                                  backgroundCustomImage1 = ""
-                                  backgroundCustomImage2 = ""
-                              }
-
-                              let values: [String: Any] = ["email" : email,
-                                            "username" : userName,
-                                            "userNickName" : userNickName,
-                                            "photoURLString" : photoURLString as Any,
-                                            "userSetting" : userSetting,
-                                            "bio" : bio,
-                                            "backgroundCustomImage1" : backgroundCustomImage1,
-                                            "backgroundCustomImage2" : backgroundCustomImage2
-                              ]
-
-                              // 로그인된 사용자 정보를 업데이트
-                            REF_USERS.child(uid!).updateChildValues(values)
-                            print("apple Login test\(uid!), values\(values)")
-                              // 다시 메인화면 보여주기
-                              guard let window = UIApplication.shared.windows.first(where: {$0.isKeyWindow}) else {
-                                  return }
-                              
-                              guard let tab = window.rootViewController as? MainTabController else {return}
-                              
-                              tab.authenticateUserAndConfigureUI()
-                              
-                              UserDefaults.standard.bool(forKey: "userLandingPageCheck")
-                              self.dismiss(animated: true, completion: nil) // 현제 present되어있는 로그인
-                          }
+                    let uid = authResult?.user.uid
+                    let email: String
+                    let userName: String
+                    let userNickName: String
+                    let photoURLString: String?
+                    let userSetting: Bool
+                    let bio: String
+                    let backgroundCustomImage1: String
+                    let backgroundCustomImage2: String
                     
+                    if let user = fetchedUser {
+                        // 사용자 정보가 이미 있는 경우
+                        email = user.email
+                        userName = user.username
+                        userNickName = user.userNickName
+                        photoURLString = user.photoURLString?.absoluteString
+                        userSetting = user.userSetting
+                        bio = user.bio ?? "Memoir Mate User"
+                        backgroundCustomImage1 = user.backgroundCustomImage1?.absoluteString ?? ""
+                        backgroundCustomImage2 = user.backgroundCustomImage2?.absoluteString ?? ""
+                    } else {
+                        // 처음 로그인하는 사용자의 경우
+                        email = authResult?.user.email ?? ""
+                        userName = authResult?.user.displayName ?? ""
+                        userNickName = ""
+                        photoURLString = authResult?.user.photoURL?.absoluteString
+                        userSetting = false
+                        bio = "Memoir Mate User"
+                        backgroundCustomImage1 = ""
+                        backgroundCustomImage2 = ""
+                    }
+                    
+                    let values: [String: Any] = ["email" : email,
+                                                 "username" : userName,
+                                                 "userNickName" : userNickName,
+                                                 "photoURLString" : photoURLString as Any,
+                                                 "userSetting" : userSetting,
+                                                 "bio" : bio,
+                                                 "backgroundCustomImage1" : backgroundCustomImage1,
+                                                 "backgroundCustomImage2" : backgroundCustomImage2
+                    ]
+                    
+                    // 로그인된 사용자 정보를 업데이트
+                    REF_USERS.child(uid!).updateChildValues(values)
+                    print("apple Login test\(uid!), values\(values)")
+                    // 다시 메인화면 보여주기
+                    guard let window = UIApplication.shared.windows.first(where: {$0.isKeyWindow}) else {
+                        return }
+                    
+                    guard let tab = window.rootViewController as? MainTabController else {return}
+                    
+                    tab.authenticateUserAndConfigureUI()
+                    
+                    UserDefaults.standard.bool(forKey: "userLandingPageCheck")
+                    self.dismiss(animated: true, completion: nil) // 현제 present되어있는 로그인
+                }
+                
             }
         }
     }
